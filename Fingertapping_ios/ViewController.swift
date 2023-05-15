9c963850059ce38eb9b6292c0dd6b15114066713
jiwan0630 <neve0630@blifeinc.com>
2023-05-15 13:04:27 +0900

    Initial Commit

---
+++ Fingertapping_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fingertapping_ios
+//
+//  Created by KJW on 2023/05/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
